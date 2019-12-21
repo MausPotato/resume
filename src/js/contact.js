ddb9a56cc2cf96b6145f9360529ceb96dd54ee11
Maus <51877347+MausPotato@users.noreply.github.com>
2019-12-21 23:15:36 +0800

    fix contact

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,7 +1,7 @@
 let sendBtn = document.querySelector("#send");
 sendBtn.addEventListener("click", send);
 
-document.querySelectorAll("#message > *").forEach((input) => {
+document.querySelectorAll("#message > input, #message > textarea").forEach((input) => {
   input.addEventListener("focus", function() {
     this.classList.remove("error");
   });
@@ -18,26 +18,36 @@ function send() {
   });
   if (status) {
     let currentTime = new Date();
-    let data = {
-      "time": currentTime.toLocaleString(),
-      "name": document.querySelector("#name").value,
-      "mail": document.querySelector("#mail").value,
-      "text": document.querySelector("#text").value
-    }
-    makeRequest(data);
+    let queryString = `time=${currentTime.toLocaleString()}&name=${document.querySelector("#name").value}&mail=${document.querySelector("#mail").value}&text=${document.querySelector("#text").value}`;
+    // let data = {
+    //   "time": currentTime.toLocaleString(),
+    //   "name": document.querySelector("#name").value,
+    //   "mail": document.querySelector("#mail").value,
+    //   "text": document.querySelector("#text").value
+    // }
+    makeRequest(queryString);
   }
+}
+
+function toQueryString(data) {
+  let params = [];
+  for (let index in data) {
+    params.push(index + "=" + data.index);
+  }
+  return params.join("&");
 }
 
 let httpRequest;
 function makeRequest(data) {
+  console.log(data);
   httpRequest = new XMLHttpRequest();
   if (!httpRequest) {
     alert('Giving up :( Cannot create an XMLHTTP instance');
     return false;
   }
   httpRequest.onreadystatechange = alertContents;
-  httpRequest.open('GET', 'https://script.google.com/macros/s/AKfycbzRS0rz8tThx1RVTffcaGv8i3mz0d1r4HB2z2IWYh7BuE45P7s4/exec');
-  httpRequest.send(data);
+  httpRequest.open('GET', `https://script.google.com/macros/s/AKfycbzRS0rz8tThx1RVTffcaGv8i3mz0d1r4HB2z2IWYh7BuE45P7s4/exec?${data}`);
+  httpRequest.send();
 }
 
 function alertContents() {
@@ -45,6 +55,9 @@ function alertContents() {
   if (httpRequest.readyState === XMLHttpRequest.DONE) {
     if (httpRequest.status === 200) {
       alert("已經收到您的訊息，會盡快回覆:)");
+      document.querySelectorAll("#message > input, #message > textarea").forEach((input) => {
+        input.value = "";
+      });
     } else {
       alert('There was a problem with the request.');
     }
